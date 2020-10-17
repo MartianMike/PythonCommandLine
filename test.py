710b4ccfc308e6c6b29aee81dc62f6168c8beba6
Mike <32580487+MartianMike@users.noreply.github.com>
2020-10-17 12:23:12 -0700

    Added new file to input 2 numbers and print out range. (#1)
    Co-authored-by: Mike <32580487+mikeorr0@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,10 @@
+# Test program
+print("Enter first number in range: ", end=' ')
+a = int(input())
+print("Enter last number in range: ", end=' ')
+b = int(input())
+
+print("\n")
+for i in range(a, b + 1):
+    print(i, end=' ')
+print("\n")
